6cea68f6281011fbc40cd9571c0f5f457fe9a390
machpich <s-obama@o-uccino.jp>
2022-10-13 19:39:54 +0900

    Add gcp

---
+++ hello/target/debug/deps/hello-5fcd4c46a90f3099.d
@@ -0,0 +1,5 @@
+/Users/s-obama/IdeaProjects/learning_rust_programming/hello/target/debug/deps/hello-5fcd4c46a90f3099: src/main.rs
+
+/Users/s-obama/IdeaProjects/learning_rust_programming/hello/target/debug/deps/hello-5fcd4c46a90f3099.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/s-obama/IdeaProjects/learning_rust_programming/hello/target/debug/hello: /Users/s-obama/IdeaProjects/learning_rust_programming/hello/src/main.rs
