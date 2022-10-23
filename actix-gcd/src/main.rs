1239d1abb7aa7a7b46e28a2a4a7a545e4dd5b1c7
machpich <s-obama@o-uccino.jp>
2022-10-23 15:54:41 +0900

    Add post

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,9 +1,17 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcp))
     });
 
     println!("Serving on http://localhost:3000...");
@@ -25,4 +33,34 @@ fn get_index() -> HttpResponse {
                 </form>
             "#
         )
+}
+
+fn post_gcp(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
