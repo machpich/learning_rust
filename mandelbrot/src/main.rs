a1ae3265de5539a63ddbfc823da275d1807d9043
machpich <s-obama@o-uccino.jp>
2022-10-26 19:51:28 +0900

    Add render fn

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn escape_time(c: Complex<u64>, limit: usize) -> Option<usize> {
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
@@ -69,9 +69,26 @@ fn pixel_to_point(bounds: (usize, usize),
 
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(pixel_to_point((100,200), (25,175),
+    assert_eq!(pixel_to_point((100, 200), (25, 175),
                               Complex { re: -1.0, im: 1.0 },
                               Complex { re: 1.0, im: -1.0 }),
                Complex { re: -0.5, im: -0.75 });
 }
 
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                       upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point,255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                }
+        }
+    }
+}
