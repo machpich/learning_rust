78f09f3d8925559744b005544633c7cb9e82f3dd
machpich <s-obama@o-uccino.jp>
2022-10-29 14:44:59 +0900

    run mandelbrot

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,12 +1,34 @@
+use std::env;
 use std::str::FromStr;
 use num::Complex;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
-use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT",
+                  args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                  args[0]);
+        std::process::exit(1);
+    }
+    
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+    
+    let mut pixels =vec![0; bounds.0 * bounds.1];
+    
+    render(&mut pixels, bounds, upper_left, lower_right);
+    
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
@@ -17,7 +39,7 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
         }
         z = z * z + c;
     }
-    None;
+    None
 }
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
