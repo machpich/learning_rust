1dc174af6037cc45bc6cc3c91f5b7e95adf8b80e
machpich <s-obama@o-uccino.jp>
2022-10-29 14:07:55 +0900

    Update madelbrot

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,5 +1,9 @@
 use std::str::FromStr;
 use num::Complex;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("Hello, world!");
@@ -85,10 +89,20 @@ fn render(pixels: &mut [u8],
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row),
                                        upper_left, lower_right);
-            pixels[row * bounds.0 + column] = match escape_time(point,255) {
-                    None => 0,
-                    Some(count) => 255 - count as u8
-                }
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            }
         }
     }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+    Ok(())
 }
